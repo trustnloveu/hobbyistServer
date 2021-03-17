119a663b0c14126ea66dddf36d1aa2156431b190
Euijin Yang <trustnloveu@gmail.com>
2021-03-17 17:52:04 +0900

    Delete Package-lock.json for Building

--- routes/categories.js
+++ routes/categories.js
@@ -26,7 +26,8 @@ router.get("/:id", validateObjectId, async (req, res) => {
 });
 
 // POST
-router.post("/", [auth, admin], async (req, res) => {
+// [auth, admin]
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
